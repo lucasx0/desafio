f56e7c21f58b2eb7263389a1123dc12432543da6
lucasx0 <lucas.almeidasilva49@gmail.com>
2023-02-17 08:54:25 -0300

    utilizando axios da forma certa.

--- marvel/src/pages/Home/home.js
+++ marvel/src/pages/Home/home.js
@@ -1,21 +1,17 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
-import md5 from 'md5';
 
-const publicKey = 'bfd388ba325f5978227228d57ef8c0cc';
-const privateKey = 'd3f3b9e0d9b32b5d2cfa8c89065f9eec231e04f6';
-const time = Number(new Date());
-const hash = md5(time + privateKey + publicKey);
+import Api from '../../services/api';
 
 export default function Home() {
   useEffect(() => {
-    axios
-      .get(
-        `http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`,
-      )
-      .then((response) => console.log(response.data.data))
-      .catch((err) => console.log(err));
+    async function loadM() {
+      const response = await Api.get();
+      console.log(response);
+    }
+
+    loadM();
   }, []);
+
   return (
     <div className="container">
       {/*  <div className="lista-filmes">
